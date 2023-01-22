15b2f4b27d3aa90fe94bffa8295369416c22004d
Kapysto4ka <115617238+Kapysto4ka@users.noreply.github.com>
2023-01-22 16:17:56 +0200

    Add files via upload

---
+++ ex1/src/SomethingIsWrong.java
@@ -0,0 +1,8 @@
+public class SomethingIsWrong {
+    public static void main(String[] args) {
+        myRect myRect = new myRect();
+        myRect.width = 40;
+        myRect.height = 5;
+        System.out.println("myRect's area is " + myRect.area());
+    }
+}
---
+++ ex1/src/myRect.java
@@ -0,0 +1,14 @@
+public class myRect {
+    int height;
+    int width;
+    int area;
+    public void height(){
+        this.height = height;
+    }
+    public void width(){
+         this.width = width;
+    }
+    public int area(){
+        return area = width * height;
+    }
+}
